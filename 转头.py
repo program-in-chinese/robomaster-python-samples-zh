8a1ed47ea449fd287039526f6a3f798dee500d2d
nobodxbodon <nobodxbodon.github@gmail.com>
2019-10-14 16:01:09 -0700

    接口中文化

--- 转头.py
+++ 转头.py
@@ -1,10 +1,19 @@
-常量 = rm_define
-大师 = robot_ctrl
-云台 = gimbal_ctrl
-装甲 = armor_ctrl
-
 def start():
-    大师.set_mode(常量.robot_mode_free)
-    云台.set_rotate_speed(260)
-    装甲.cond_wait(常量.cond_armor_bottom_back_hit)
-    云台.yaw_ctrl(10)
+    大师.设置模式(常量.自由模式)
+    云台.设置旋转速度(260)
+    装甲.等待时机(常量.装甲被袭_后底)
+    云台.平转(20)
+
+常量 = rm_define
+常量.自由模式 = 常量.robot_mode_free
+常量.装甲被袭_后底 = 常量.cond_armor_bottom_back_hit
+
+大师 = robot_ctrl
+大师.设置模式 = 大师.set_mode
+
+云台 = gimbal_ctrl
+云台.设置旋转速度 = 云台.set_rotate_speed
+云台.平转 = 云台.yaw_ctrl
+
+装甲 = armor_ctrl
+装甲.等待时机 = 装甲.cond_wait
