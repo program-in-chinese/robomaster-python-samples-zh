2674d0efdeb33a81f8404d37eee6b62fa4be2f55
nobodxbodon <nobodxbodon.github@gmail.com>
2019-10-25 00:48:16 -0700

    虽退出, 但报错

--- Python入门/源码/谁打了我.py
+++ Python入门/源码/谁打了我.py
@@ -1,16 +1,20 @@
 def start():
-    次数 = 0
+    装甲.设置灵敏度(8)
     while True:
         云台.俯仰(30)
         云台.俯仰(-10)
 
-def armor_hit_detection_all(msg):
+def 装甲被袭(msg):
     print('got hit')
-    rmexit()
+    退出()
 
 # 以下为API中文化部分, 与程序逻辑无关. 请勿作修改.
 
 云台 = gimbal_ctrl
 云台.俯仰 = 云台.pitch_ctrl
 
-退出 = rmexit()
+装甲 = armor_ctrl
+装甲.设置灵敏度 = 装甲.set_hit_sensitivity
+
+退出 = rmexit
+armor_hit_detection_all = 装甲被袭
