da5cba5f34f327e8864183feec28aeffbb0985e7
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-10-28 14:33:31 -0700

    使用相同函数结构

--- Python API视频演示与例程/系统/原始计时.py
+++ Python API视频演示与例程/系统/原始计时.py
@@ -4,10 +4,15 @@ import time
 def bench():
     t1 = time.time()
     product = 1.0
-    for counter in range(1, 1000, 1):
-        for dex in list(range(1, 360, 1)):
-            angle = radians(dex)
-            product *= sin(angle)**2 + cos(angle)**2
+    for counter in range(1, 1001):
+        product *= calculate()
     t2 = time.time() - t1
     print(t2)
+    return product
+
+def calculate():
+    product = 1.0
+    for dex in range(1, 360):
+        angle = radians(dex)
+        product *= sin(angle)**2 + cos(angle)**2
     return product
