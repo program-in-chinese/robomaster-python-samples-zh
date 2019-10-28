8b20533aff3a666e9edbd4567a458c85b66b401f
nobodxbodon <nobodxbodon.github@gmail.com>
2019-10-28 14:47:27 -0700

    i5

--- Python API视频演示与例程/系统/原始计时.py
+++ Python API视频演示与例程/系统/原始计时.py
@@ -1,6 +1,7 @@
 # 源于 https://gist.github.com/apalala/3fbbeb5305584d2abe05
 
 # Intel Core i7-2635QM @ 2.00GHz 0.2929880619049072 秒
+# i5-2410m 2.3GHz 0.6440367698669434 秒
 
 from math import sin, cos, radians
 import time
