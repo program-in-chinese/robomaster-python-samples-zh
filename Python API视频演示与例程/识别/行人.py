aef378abbcd402d507c0bfeb9f15e8c20c02b8f1
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-11-06 10:45:35 -0800

    初测

---
+++ Python API视频演示与例程/识别/行人.py
@@ -0,0 +1,53 @@
+def 开始():
+    vision_ctrl.enable_detection(rm_define.vision_detection_people)
+    
+    while True:
+        if vision_ctrl.check_condition(rm_define.cond_recognized_people):
+            行人信息 = vision_ctrl.get_people_detection_info()
+            print(行人信息)
+        时间.睡眠(1)
+        
+
+# 以下为API中文化部分, 与程序逻辑无关. 请勿作修改.
+
+start = 开始
+
+工具 = tools
+工具.取上电时间 = tools.get_unixtime
+
+LED灯 = led_ctrl
+LED灯.云台 = LED灯.set_top_led
+
+大师 = robot_ctrl
+大师.设置模式 = 大师.set_mode
+
+云台 = gimbal_ctrl
+云台.设置旋转速度 = 云台.set_rotate_speed
+云台.平转 = 云台.yaw_ctrl
+云台.俯仰 = 云台.pitch_ctrl
+
+底盘 = chassis_ctrl
+底盘.设置旋转速度 = 底盘.set_rotate_speed
+底盘.旋转指定角度 = 底盘.rotate_with_degree
+
+# 常量部分
+常量 = rm_define
+常量.底盘跟随模式 = 常量.robot_mode_chassis_follow
+常量.云台跟随模式 = 常量.robot_mode_gimbal_follow
+常量.自由模式 = 常量.robot_mode_free
+
+常量.顺时针 = 常量.clockwise
+常量.逆时针 = 常量.anticlockwise
+
+常量.云台所有 = 常量.armor_top_all
+常量.云台左 = 常量.armor_top_left
+常量.云台右 = 常量.armor_top_right
+
+常量.效果常亮 = 常量.effect_always_on
+常量.效果熄灭 = 常量.effect_always_off
+常量.效果呼吸 = 常量.effect_breath
+常量.效果闪烁 = 常量.effect_flash
+常量.效果走马灯 = 常量.effect_marquee
+
+时间 = time
+时间.睡眠 = 时间.sleep
